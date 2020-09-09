8f91e1982c7b80c2ddb456f510845411ab094a3d
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-09-09 11:33:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ main.remove();
 
 // creates newHeader variable
 const newHeader = document.createElement("h1");
-// gives newHeader an id of victory 
+// gives newHeader an id of victory
 newHeader.id="victory"
 newHeader.innerHTML = "Flatiron School is the champion!";
 
