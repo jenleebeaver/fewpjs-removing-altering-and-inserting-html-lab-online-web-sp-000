2ea0ebd59cf8085805d2b4e49e435c2db6d0d14d
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-09-09 11:32:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ main.remove();
 
 // creates newHeader variable
 const newHeader = document.createElement("h1");
+// gives newHeader an id of victory 
 newHeader.id="victory"
 
 // /* Create your new element here and assign it to newHeader */
