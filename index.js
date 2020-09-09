55ff1b4ded84cc836368f79fb62d9ac800208afe
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-09-09 11:34:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,8 +8,4 @@ const newHeader = document.createElement("h1");
 newHeader.id="victory"
 newHeader.innerHTML = "Flatiron School is the champion!";
 
-// /* Create your new element here and assign it to newHeader */
-// const newHeader = document.createElement("h1");
-// newHeader.id = "victory"
-// newHeader.innerHTML = "Flatiron School is the champion!";
-// newHeader.className = "victory";
+
